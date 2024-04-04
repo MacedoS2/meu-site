28884e31543814d1c76661ef0ad73f4be3adb15a
MacedoS2 <160137200+MacedoS2@users.noreply.github.com>
2024-04-04 09:26:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll("aba-conteudo");
+console.log(botoes)
+for (let i=0;i <botoes.lenght; i++){
+    botoes(i).onclik = function() {
+    
+        for(let j=0;j<botoes.lenght;j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo")
+        }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo")
+    }
+}   
+const contadores = document.querySelectorAll("contador");
+contadores[]
